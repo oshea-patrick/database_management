2d90a76411d1c537175185230c88ed483628d10d
Patrick OShea <Patrick O'Shea>
2021-04-15 17:39:44 -0400

    Example

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,25 @@ import React, { useState, useEffect } from 'react';
 import '../styles/All.css'
 
 function Home () {
+    const [count, setCount] = useState(0)
+    const [numbers, setNumbers] = useState([0])
+
+    function increment () {
+        setNumbers([...numbers, count + 1])
+        setCount(count + 1)
+    }
+
     return (
         <div className="body">
+            <h1>Hello {count} </h1>
+            <input type="button" onClick={ () => { increment() }} value="Click Me"/>
+            {numbers.map((number) => (
+            <div>
+                <strong className="glow">{number}</strong>
+                <br/>
+            </div>
+            ))
+            }
             <div className="div-block-4">
                 <h1 className="heading">Welcome to punch-in!<br/>How can we help<br/>you?</h1> <a href="/inventory" className="button">Inventory</a> <a href="/reservations" className="button">Reservations</a>
             </div>
